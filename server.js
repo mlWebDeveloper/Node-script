29d632749f402bd449b68380c575836b8b599f7d
fineselect <fineselect@live.com>
2016-05-19 21:51:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,43 +1,45 @@
 //Lets require/import the HTTP module
 
-(function () {
-	var http = require('http'),
-		fs = require('fs'),
-		path = require('path'), 
-		host = '127.0.0.1',
-		port = '3000';
+(function() {
+  var http = require('http'),
+    fs = require('fs'),
+    path = require('path'),
+    host = '127.0.0.1',
+    port = '3000';
 
-	var mimes = {
-		"htm" 	: "text/html",
-		"html" 	: "text/html",
-		".css" 	: "text/css",
-		".js" 	: "text/javascript",
-		".json" : "application/json",
-		".gif" 	: "image/gif",
-		".jpg" 	: "image/jpeg",
-		".png" 	: "image/png"
-	}
+  var mimes = {
+    "htm": "text/html",
+    "html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".json": "application/json",
+    ".gif": "image/gif",
+    ".jpg": "image/jpeg",
+    ".png": "image/png"
+  }
 
-	var server = http.createServer(function(req, res){
-		var filepath = (req.url === '/') ? ('./index.html') : ('.' + req.url);
-		var ContentType = mimes[path.extname(filepath)];
+  var server = http.createServer(function(req, res) {
+    var filepath = (req.url === '/') ? ('./index.html') : ('.' + req.url);
+    var ContentType = mimes[path.extname(filepath)];
 
-		fs.exists(filepath, function (file_exists) {
-			if (file_exists) {
-				res.writeHead(200, {'Content-Type' : ContentType});
-				var streamFile = fs.createReadStream(filepath).pipe(res);
+    fs.exists(filepath, function(file_exists) {
+      if (file_exists) {
+        res.writeHead(200, {
+          'Content-Type': ContentType
+        });
+        var streamFile = fs.createReadStream(filepath).pipe(res);
 
-				streamFile.on('error', function () {
-					res.writeHead(500);
-					res.end();
-				})
-			} else {
-				res.writeHead(404);
-				res.end("Sorry we could not find the file you requested.");
-			}
-		})
-	}).listen(port, host, function(){
-		console.log('\n' +'Success !!!');
-		console.log('Server running on http://' + host + ':' + port);
-	});
+        streamFile.on('error', function() {
+          res.writeHead(500);
+          res.end();
+        })
+      } else {
+        res.writeHead(404);
+        res.end("Sorry we could not find the file you requested.");
+      }
+    })
+  }).listen(port, host, function() {
+    console.log('\n' + 'Success !!!');
+    console.log('Server running on http://' + host + ':' + port);
+  });
 })();
